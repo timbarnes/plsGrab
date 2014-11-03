99abe186ecb1b155cede529eb06b8f3ec2ccfc1a
Tim Barnes <tw@timandmarina.com>
2014-11-02 21:26:19 -0800

    Style updated to PEP-8

--- plsgrab.py
+++ plsgrab.py
@@ -2,69 +2,76 @@
 
 # Read in a .pls file from the Internet, and print out the URL of the stream for mpd
 
-import sys, os
-import urllib, urllib2
+import sys
+import os
+
+import urllib2
 from re import sub, search, findall
 
 
 # Get the value of an external DEBUG shell variable to determine if we should print debug info
 try:
-    DebugMode = int(os.environ["DEBUG"])
+    DEBUG_MODE = int(os.environ["DEBUG"])
 except:
-    DebugMode = 0
+    DEBUG_MODE = 0
+
+
+def D(level, string, object):
+    """Print debug information if the debug level is set
+    """
     
-def DEBUG(level, string, object):
-    """Print debug information if the debug level is set"""
-    
-    if DebugMode >= level:
+    if DEBUG_MODE >= level:
         print string, ':', object
     return
 
-def getOneStreamURL(URL):
-    """Extracts the first stream URL from the given .pls file"""
+
+def get_one_stream_url(url):
+    """Extracts the first stream URL from the given .pls file
+    """
     
-    DEBUG(1, 'getOneStreamURL looking for', URL)
+    D(1, 'get_one_stream_url looking for', url)
     try:
-        response = urllib2.urlopen(URL)
+        response = urllib2.urlopen(url)
         html = response.read()
-        DEBUG(2, 'plsStream HTML', html)
+        D(2, 'plsStream HTML', html)
         stream = search(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', html)
-        DEBUG(1, 'getOneStreamURL found', stream.group())
+        D(1, 'get_one_stream_url found', stream.group())
         if stream:
             stream = sub(r'(&|\?).*', '', stream.group())
-            DEBUG(1, 'returning', stream)
+            D(1, 'returning', stream)
             return stream
-        DEBUG(1, 'getOneStreamURL', 'not found')
-    except urllib2.URLError as e:
+        D(1, 'get_one_stream_url', 'not found')
+    except urllib2.urlError as e:
         print e.reason
     return
 
 
-def getStreamURLs(URL):
+def get_stream_urls(url):
     """If given a .pls URL, use regex to extract the URL up to the ? or end, whichever happens first.
-    If a non-pls file, search the file for .pls entries, and process each."""
+    If a non-pls file, search the file for .pls entries, and process each.
+    """
 
-    responseList = []
-    if URL[-4:] == '.pls':
-        DEBUG(1, 'Single Pls', URL)
-        responseList.append(getOneStreamURL(URL))
-        DEBUG(1, 'getStreamURLs found', responseList)
-        return responseList
+    response_list = []
+    if url[-4:] == '.pls':
+        D(1, 'Single Pls', url)
+        response_list.append(get_one_stream_url(url))
+        D(1, 'get_stream_urls found', response_list)
+        return response_list
     else:
-        DEBUG(1, 'getStreamURLs looking for multiple in', URL)
+        D(1, 'get_stream_urls looking for multiple in', url)
         try:
-            response = urllib2.urlopen(URL)
+            response = urllib2.urlopen(url)
             html = response.read()
-            plsURLs = findall(r'(http://.*\.pls)', html)
-            DEBUG(1, 'getStreamURLs found:', plsURLs)
-            for plsURL in plsURLs:
-                DEBUG(1, 'getStreamURLs looking for', plsURL)
-                plsURL = sub(r'.pls.*', '.pls', plsURL)
-                streamURL = getOneStreamURL(plsURL)
-                if streamURL:
-                    responseList.append(streamURL)
-            return responseList
-        except urllib2.URLError as e:
+            pls_urls = findall(r'(http://.*\.pls)', html)
+            D(1, 'get_stream_urls found:', pls_urls)
+            for pls_url in pls_urls:
+                D(1, 'get_stream_urls looking for', pls_url)
+                pls_url = sub(r'.pls.*', '.pls', pls_url)
+                streamurl = get_one_stream_url(pls_url)
+                if streamurl:
+                    response_list.append(streamurl)
+            return response_list
+        except urllib2.urlError as e:
             print e.reason
     return
     
@@ -77,11 +84,12 @@ def main():
         sys.exit(1)
 
     for arg in args:
-        DEBUG(1, 'main', args)
-        URLs = getStreamURLs(arg)
-        DEBUG(1, 'main result', URLs)
-        for URL in URLs:
-            print URL
-        
+        D(1, 'main', args)
+        urls = get_stream_urls(arg)
+        D(1, 'main result', urls)
+        for url in urls:
+            print url
+
+
 if __name__ == '__main__':
   main()
